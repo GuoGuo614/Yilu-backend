17ed48fe9439866d9327994b2533e114de6717c3
GuoGuo614 <2251270969@qq.com>
2025-03-14 15:55:44 +0800

    01， 10

--- backend_0010/topic_1/Perarea.java
+++ backend_0010/topic_1/Perarea.java
@@ -4,4 +4,15 @@ package topic_1;
 public interface Perarea {
     double get_area();
     double get_perimeter();
+
+    // Test
+    static void main(String[] args) {
+        Perarea rect = new Rectangle();
+        Perarea circle = new Circle();
+        System.out.print("The area and perimeter of the rectangle is "
+                + rect.get_area() + " and " + rect.get_perimeter());
+        System.out.println();
+        System.out.print("The area and perimeter of the circle is "
+                + circle.get_area() + " and " + circle.get_perimeter());
+    }
 }
